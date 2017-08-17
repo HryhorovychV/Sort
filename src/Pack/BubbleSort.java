b7276f2b6c5c303366f06229bd9f582cfdf4dce0
HryhorovychV <31042867+HryhorovychV@users.noreply.github.com>
2017-08-17 19:06:22 +0300

    Made >

--- src/Pack/BubbleSort.java
+++ src/Pack/BubbleSort.java
@@ -2,25 +2,40 @@ package Pack;
 
 public class BubbleSort {
 
-	void Bubble(int [] items){
+	 int[] Bubble(int [] items){
 		int change;
 		for(int i =0; i< items.length; i++){
-			for(int j= 1; j < items.length-1; j++){
-				if(items[i] > items[j])
+			for(int j= 1, b = 0; j < items.length; j++, b++){
+				if(items[b] > items[j])
 				{
-					change = items[j];
-					items[j]=items[i]; 
-					items[i]= change;
+					change = items[b];
+					items[b]=items[j]; 
+					items[j]= change;
 				}
+				
 				
 			}
 		}
 		
+		return items;
 	}
+		
+	void Print(int [] items)
+		{
+			for(int i =0; i< items.length; i++)
+			{
+				System.out.println(items[i]);
+			}
+		}
+		
+	
 	
 	
 	public static void main(String[] args) {
-
+		BubbleSort s = new BubbleSort();
+		int [] b = new int [] {9, 6, 4, 3};
+		int [] a = s.Bubble(b);
+        s.Print(a);
 	}
 
 }
