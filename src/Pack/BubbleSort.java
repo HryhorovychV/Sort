3c84a960c35f8b296ea85bd7116bc6a9a06f5d39
HryhorovychV <31042867+HryhorovychV@users.noreply.github.com>
2017-08-17 18:29:38 +0300

    Add Bubble for >

--- src/Pack/BubbleSort.java
+++ src/Pack/BubbleSort.java
@@ -2,8 +2,24 @@ package Pack;
 
 public class BubbleSort {
 
+	void Bubble(int [] items){
+		int change;
+		for(int i =0; i< items.length; i++){
+			for(int j= 1; j < items.length-1; j++){
+				if(items[i] > items[j])
+				{
+					change = items[j];
+					items[j]=items[i]; 
+					items[i]= change;
+				}
+				
+			}
+		}
+		
+	}
+	
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
 	}
 
